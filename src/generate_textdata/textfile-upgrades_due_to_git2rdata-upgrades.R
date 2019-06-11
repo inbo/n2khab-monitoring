572c730eddcc6922cc1c69005bebc21730e84e20
florisvdh <floris.vanderhaeghe@inbo.be>
2019-06-11 16:50:31 +0200

    Update paths in script to upgrade vc-formatted data

--- src/generate_textdata/textfile-upgrades_due_to_git2rdata-upgrades.R
+++ src/generate_textdata/textfile-upgrades_due_to_git2rdata-upgrades.R
@@ -2,7 +2,7 @@
 # upgrades of the git2rdata package that change the way files are written.
 # Its repository is at https://github.com/inbo/git2rdata
 
-# The used functions runs from the folder src/generate_textdata
+# The used functions runs from the root of the repo
 
 getwd()
 
@@ -12,12 +12,12 @@ getwd()
 # Previous state of git2rdata: commit 4b1f2fd
 # Conversion to new state of git2rdata: commit df1ba32 (tag v0.0.4)
 
-git2rdata::upgrade_data(path = "rawraw_data")
+git2rdata::upgrade_data(path = "src/generate_textdata/rawraw_data")
     # rawraw_data/10_compmeetnet_types_milieudrukken.yml updated
     # rawraw_data/10_compmeetnet_types_milieudrukken.yml
     # "rawraw_data/10_compmeetnet_types_milieudrukken"
 
-git2rdata::upgrade_data(path = "../../n2khabutils/inst/textdata")
+git2rdata::upgrade_data(path = "n2khabutils/inst/textdata")
     # ../../n2khabutils/inst/textdata/env_pressures.yml updated
     # ../../n2khabutils/inst/textdata/namelist.yml updated
     # ../../n2khabutils/inst/textdata/schemes.yml updated
