3ffb667b198c43efec62caf6462f305e862d663c
florisvdh <floris.vanderhaeghe@inbo.be>
2024-08-27 10:14:26 +0200

    RStudio project: reset tab width

--- R/planning_helper.R
+++ R/planning_helper.R
@@ -2,10 +2,10 @@
 # .Renviron and make sure you ran googledrive::drive_auth() interactively with
 # these settings for the first run (or to renew an expired Oauth token)
 if (Sys.getenv("GARGLE_OAUTH_EMAIL") != "") {
-    options(gargle_oauth_email = Sys.getenv("GARGLE_OAUTH_EMAIL"))
+  options(gargle_oauth_email = Sys.getenv("GARGLE_OAUTH_EMAIL"))
 }
 if (Sys.getenv("GARGLE_OAUTH_CACHE") != "") {
-    options(gargle_oauth_cache = Sys.getenv("GARGLE_OAUTH_CACHE"))
+  options(gargle_oauth_cache = Sys.getenv("GARGLE_OAUTH_CACHE"))
 }
 
 library(googlesheets4)
@@ -25,82 +25,83 @@ df <- read_sheet(
   .name_repair = "minimal"
 )
 
-df_long <- df |>
-    clean_names() |>
-    select(
-      -uitvoerders,
-      -reviewers,
-      -starts_with("totaal_dagen"),
-      -ends_with("_buiten_kern"),
-      -starts_with("tijdsinvestering")
-    ) |>
-    rename_with(
-        .cols = karen:datamanager,
-        .fn = \(x) {str_c(x, "_1")}
-    ) |>
-    mutate(
-        continuous = deadline == "Doorlopend",
-        deadline = ifelse(deadline == "Doorlopend", "2025-12", deadline),
-        start = ym(start),
-        deadline = ym(deadline) + months(1) - days(1),
-        nr_months = as.period(deadline - start) |>
-            as.numeric("months") |>
-            round() |>
-            as.integer()
-    ) |>
-    uncount(nr_months, .remove = FALSE) |>
-    mutate(
-        date = start + months(row_number() - 1),
-        y_month = str_c(year(date), "_", str_pad(month(date), 2, pad = "0")) |>
-            factor(),
-        .by = taakomschrijving
-    ) |>
-    pivot_longer(
-      cols = karen_1:datamanager_2,
-      names_to = c(".value", "task_type"),
-      names_pattern = "(.+)_([12])$"
-    ) |>
-    pivot_longer(
-        karen:datamanager,
-        names_to = "person",
-        values_to = "nr_days",
-        values_drop_na = TRUE
-    ) |>
-    mutate(
-        task_type = ifelse(task_type == "1", "uitvoering", "review") |> fct(),
-        nr_days = ifelse(
-            continuous,
-            nr_days,
-            nr_days / nr_months
-        ),
-        across(where(is.character), fct)
-    ) |>
-    select(-nr_months)
+df_long <-
+  df |>
+  clean_names() |>
+  select(
+    -uitvoerders,
+    -reviewers,
+    -starts_with("totaal_dagen"),
+    -ends_with("_buiten_kern"),
+    -starts_with("tijdsinvestering")
+  ) |>
+  rename_with(
+    .cols = karen:datamanager,
+    .fn = \(x) {str_c(x, "_1")}
+  ) |>
+  mutate(
+    continuous = deadline == "Doorlopend",
+    deadline = ifelse(deadline == "Doorlopend", "2025-12", deadline),
+    start = ym(start),
+    deadline = ym(deadline) + months(1) - days(1),
+    nr_months = as.period(deadline - start) |>
+      as.numeric("months") |>
+      round() |>
+      as.integer()
+  ) |>
+  uncount(nr_months, .remove = FALSE) |>
+  mutate(
+    date = start + months(row_number() - 1),
+    y_month = str_c(year(date), "_", str_pad(month(date), 2, pad = "0")) |>
+      factor(),
+    .by = taakomschrijving
+  ) |>
+  pivot_longer(
+    cols = karen_1:datamanager_2,
+    names_to = c(".value", "task_type"),
+    names_pattern = "(.+)_([12])$"
+  ) |>
+  pivot_longer(
+    karen:datamanager,
+    names_to = "person",
+    values_to = "nr_days",
+    values_drop_na = TRUE
+  ) |>
+  mutate(
+    task_type = ifelse(task_type == "1", "uitvoering", "review") |> fct(),
+    nr_days = ifelse(
+      continuous,
+      nr_days,
+      nr_days / nr_months
+    ),
+    across(where(is.character), fct)
+  ) |>
+  select(-nr_months)
 
 summarize_planning <- function(x,
                                priorities = 1,
                                max_year = 2025,
                                tempres = c("y_month", "y"),
                                include_continuous = TRUE) {
-    tempres <- if (!is.null(tempres)) (match.arg(tempres))
-    x |>
-        mutate(y = year(date)) |>
-        filter(
-          prioriteit %in% priorities,
-          y <= max_year,
-          include_continuous | !continuous,
-          # generieke_omschrijving == "Aanwervingen"
-        ) |>
-        summarize(
-            days = sum(nr_days),
-            .by = c(person, {{tempres}})
-        ) |>
-        arrange(person, {{tempres}}) |>
-        pivot_wider(
-          names_from = person,
-          values_from = days,
-          names_sort = TRUE
-        )
+  tempres <- if (!is.null(tempres)) (match.arg(tempres))
+  x |>
+    mutate(y = year(date)) |>
+    filter(
+      prioriteit %in% priorities,
+      y <= max_year,
+      include_continuous | !continuous,
+      # generieke_omschrijving == "Aanwervingen"
+    ) |>
+    summarize(
+      days = sum(nr_days),
+      .by = c(person, {{tempres}})
+    ) |>
+    arrange(person, {{tempres}}) |>
+    pivot_wider(
+      names_from = person,
+      values_from = days,
+      names_sort = TRUE
+    )
 }
 
 # for checking (use temporary view to filter non-continuous tasks in gsheet):
