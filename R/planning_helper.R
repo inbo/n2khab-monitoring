d0f5b82997fc02aa8c8bc01694e745dbc8e01225
florisvdh <floris.vanderhaeghe@inbo.be>
2024-08-28 15:41:10 +0200

    planning_helper.R: restructure

--- R/planning_helper.R
+++ R/planning_helper.R
@@ -1,5 +1,3 @@
-# Part to run iteratively -------------------------------------------------
-
 # Setup for googledrive authentication. Set the appropriate env vars in
 # .Renviron and make sure you ran googledrive::drive_auth() interactively with
 # these settings for the first run (or to renew an expired Oauth token)
@@ -26,19 +24,22 @@ pl_long <- get_planning_long()
 # Get number number of available days as long-table format (person x month)
 avail_long <- get_availability_long()
 
+# Below sheet updates can be picked as desired, or just be run all together
+# ###########################################################################
+
 # Update the reordered planning tables per person in the planning googlesheet
 update_person_sheets(pl_long)
-
-# Update the priority_xxx sheets in the planning googlesheet
-update_priority_sheets(pl_long)
 
 # Update priority_1_avail sheet with number of days left per person & month
 update_priority_1_avail_sheet(pl_long, avail_long)
 
+# Update the priority_xxx sheets in the planning googlesheet
+update_priority_sheets(pl_long)
 
 
 
-# For checks only ---------------------------------------------------------
+
+# Don't run below; it's for checks only --------------------------------------
 
 # (use temporary view to filter non-continuous tasks in gsheet)
 
